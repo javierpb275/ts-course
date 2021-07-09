1c0826ec04ee6243fb81f95fbaf565b425fc5043
javierpb275 <javierpb275@gmail.com>
2021-07-09 22:14:06 +0200

    types: arrays and tuples

---
+++ 1.types/3-arrays-and-tuples.ts
@@ -0,0 +1,5 @@
+//arrays
+let weekDays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'saturday', 'sunday'];
+
+//tuples
+let db: [name: string, age: number, country: string, premium: boolean] = ['pepe', 29, 'Spain', true];
