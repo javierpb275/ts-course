55cb27bb01e6859a355a2f2787937b7131ba7514
javierpb275 <javierpb275@gmail.com>
2021-07-09 22:01:41 +0200

    special types

---
+++ 1.types/2-special-types.ts
@@ -0,0 +1,16 @@
+/*any*/
+let whatever: any;
+whatever = "whatever";
+whatever = 123;
+whatever = false;
+
+/*null*/
+let empty: null = null;
+
+/*undefined*/
+let notdefined: undefined = undefined;
+
+/*void*/
+function hello(): void {
+    console.log('hello');
+}
