c438c66eb3cc90d5f8df4b209ccc5ddf99a6c3c9
javierpb275 <javierpb275@gmail.com>
2021-07-10 12:53:55 +0200

    functions: basics

---
+++ 2.functions/1-calculator.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    var sum = a + b;
+    console.log(a + " + " + b + " = " + sum);
+    return sum;
+}
+var x = sum(1, 2);
---
+++ 2.functions/1-calculator.ts
@@ -0,0 +1,7 @@
+function sum(a:number, b:number):number {
+    let sum = a + b;
+    console.log(`${a} + ${b} = ${sum}`);
+    return sum;
+}
+
+let x: number = sum(1,2);
