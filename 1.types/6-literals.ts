c840ef8fb62a1b18cca7cdc9bb8207b2e297b3a1
javierpb275 <javierpb275@gmail.com>
2021-07-11 13:02:31 +0200

    types: literals

---
+++ 1.types/6-literals.ts
@@ -0,0 +1,20 @@
+let verdad: true = true;
+
+let five: 5 = 5;
+
+let hello: 'hello' = 'hello';
+
+type LoginOperation = {
+    op: 'login',
+    username: string,
+    password: string
+}
+
+let l: LoginOperation = {
+    op: 'login',
+    username: 'admin',
+    password: 'admin'
+}
+
+//This is like when you declare a const:
+const hello2 = 'hello2';
